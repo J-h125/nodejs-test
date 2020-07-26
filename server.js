def11f47fcfc892485091d87f1b364bb9a645475
J-h125 <62388502+J-h125@users.noreply.github.com>
2020-07-26 23:19:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,18 +20,34 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
-
+    
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`
+    <!DOCTYPE html>
+    <head>
+    <link rel="stylesheet" href="/x"
+    </head>
+    <body>
+    <h1>标题</h1>
+    <script src="/y"></script>
+    </body>
+
+    `)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(`body{color: red;}`)
     response.end()
-  } else {
+  } else if(path === '/y'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+    response.write(`console.log('这是JS内容')`)
+    response.end()
+  }
+    else{
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`你输入的路径不存在对应的内容`)
@@ -43,5 +59,3 @@ var server = http.createServer(function(request, response){
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
-
